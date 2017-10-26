2df9c5ddb5892a661a69b603036fda711b6c3ac4
Todd <dtodd5092@greenville.k12.sc.us>
2017-10-26 09:35:41 -0400

    added code

--- 3.py
+++ 3.py
@@ -30,10 +30,20 @@ def draw_star(t, x, y, points, line, fill):
         t.left (turn)
     t.end_fill()
 
-def draw_house(t, x, y):
+def draw_grass(t, x, y, line, fill):
     t.penup()
     t.goto(x, y)
     t.pendown()
+    t.color(line, fill)
+    t.begin_fill()
+    
+def draw_house(t, x, y, line, fill):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.color(line, fill)
+    t.begin_fill()
+    
     t.forward(350)
     t.right(90)
     t.forward(350)
@@ -45,7 +55,9 @@ def draw_house(t, x, y):
     t.forward(225)
     t.right(79)
     t.forward(231)
-
+        
+    t.end_fill()
+        
 def draw_window(t, x, y):
     t.penup()
     t.goto(x, y)
@@ -75,22 +87,37 @@ def draw_door(t, x, y):
     t.goto(x, y)
     t.pendown()
     t.right(90)
-    t.forward(150)
+    t.forward(110)
     t.left(90)
     t.forward(130)
     t.left(90)
-    t.forward(50)
+    t.forward(70)
     t.left(90)
     t.forward(150)
-   
-        
     
-    
-def draw_fence(t, x, y):
+def draw_tree(t, x, y):
     t.penup()
     t.goto(x, y)
     t.pendown()
-    forward(10)
+    t.left(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(80)
+    t.right(118)
+    t.forward(265)
+    t.right(123)
+    t.forward(268)
+    t.right(119)
+    t.forward(90)
+    
 def draw_mailbox(t, x, y):
     t.penup()
     t.goto(x, y)
@@ -100,16 +127,16 @@ def draw_mailbox(t, x, y):
 
     
 '''
-draw_star(jade, 0, 390, 36, 'orange', 'blue')
-draw_star(jazmine, -250, 390, 18,  'pink', 'purple')
+draw_star(jade, 0, 390, 36, 'orange', 'yellow')
+draw_star(jazmine, -250, 390, 18,  'orange', 'yellow')
 draw_star(meower, -500, 390, 20, 'orange', 'yellow')
-draw_star(jade, 250, 390, 12, 'blue', 'pink')
+draw_star(jade, 250, 390, 12, 'orange', 'yellow')
 '''
-
-draw_house(jade, 100, 0)
+draw_grass(meower, -500, -350)
+draw_house(jade, 100, 0, 'black', 'white')
 draw_window(jade, 150, -48)
 draw_door(jade, 200, -350)
-draw_fence(meower, -200, 0)
+draw_tree(meower, -250, -350)
 draw_mailbox(jazmine, 0, 0)
 
 
